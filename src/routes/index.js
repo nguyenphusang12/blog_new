bc28a71f80e8f0aed1454dceeb413afa8f1063c4
nguyenphusang12 <nguyenphusang.pnl@gmail.com>
2021-09-01 11:11:15 +0700

    add new

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const newRouter = require('./news');
 const siteRouter = require('./site');
-const ad = 1;
 
 function route(app) {
     app.use('/news', newRouter);
+
     app.use('/', siteRouter);
 }
 
