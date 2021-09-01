c02418fb12e7269bfc0128c982527af5952ad36b
nguyenphusang12 <nguyenphusang.pnl@gmail.com>
2021-09-01 11:10:36 +0700

    add new

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ const path = require('path');
 const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined'));
 app.engine('.hbs', hdb({ extname: '.hbs' }));
