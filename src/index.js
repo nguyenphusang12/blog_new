feb73c50295193f4ca371a18a6583f0e9351c007
nguyenphusang12 <nguyenphusang.pnl@gmail.com>
2021-09-01 11:15:44 +0700

    add new

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ const port = 8888;
 const path = require('path');
 const route = require('./routes');
 
+const object1 = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+
 app.use(morgan('combined'));
+
 app.engine('.hbs', hdb({ extname: '.hbs' }));
+
 app.set('view engine', '.hbs');
+
 app.set('views', path.join(__dirname, 'resource/views'));
 
 //route init
 route(app);
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
